05c89de98bf304bc76d5caa124585d9228e08198
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-07-24 21:46:12 +0600

    outlet Added

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header.js';
-import Shop from '../components/Shop/Shop.js';
+import { Outlet } from 'react-router-dom';
+
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-             <Shop></Shop>
+            <Outlet></Outlet>
         </div>
     );
 };
